5f2b4bb16094cb1c5b3b51542a72960669962141
Sriram Thota <111224159+sriramthota1@users.noreply.github.com>
2025-04-21 21:48:02 -0400

    Update Profile.js

--- frontend/patient-portal/src/components/Profile.js
+++ frontend/patient-portal/src/components/Profile.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Profile.css';
+import './Dashboard.js';
 
 const Profile = () => {
   return (
     <div className="profile-container">
       <h2>Profile Page</h2>
-      <p>Welcome to your profile! Manage your details here.</p>
+      <p>Welcome to your profile! Manage your details in here.</p>
     </div>
   );
 }
